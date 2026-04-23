469b1e95944afde2a85030818f5ef96508bd5f66
kunal-sharma-2803 <kunal2803sharma@gmail.com>
2026-04-23 18:35:54 +0530

    Removed env from tracking

--- sample_project1/env/etc/conda/test-files/zipp/1/tests/compat/py310.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-if sys.version_info >= (3, 11):
-    from importlib.resources.abc import Traversable
-else:  # pragma: no cover
-    from .py38 import Traversable
-
-
-__all__ = ['Traversable']
--- sample_project1/env/pyuic6.bat
+++
@@ -1,1 +0,0 @@
-@C:/Users/kunal/Desktop/AI_ML/sample_project1/env\python.exe -m PyQt6.uic.pyuic %1 %2 %3 %4 %5 %6 %7 %8 %9
